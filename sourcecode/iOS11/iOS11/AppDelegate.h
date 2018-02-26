7eee77adddf4590bca14ab569eb3e288b807d4fd
sunjinshuai <s_mike@163.com>
2018-02-26 14:10:02 +0800

    iOS11新特性

---
+++ sourcecode/iOS11/iOS11/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOS11
+//
+//  Created by sunjinshuai on 2018/2/26.
+//  Copyright © 2018年 iOS11. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ sourcecode/iOS11/iOS11/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iOS11
+//
+//  Created by sunjinshuai on 2018/2/26.
+//  Copyright © 2018年 iOS11. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
