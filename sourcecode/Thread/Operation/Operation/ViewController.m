c9f5f20944496983a34d3c950468631f7c0fa91e
sunjinshuai <s_mike@163.com>
2019-10-23 11:17:50 +0800

    addExecutionBlock

--- sourcecode/Thread/Operation/Operation/ViewController.m
+++ sourcecode/Thread/Operation/Operation/ViewController.m
@@ -17,7 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
-    [self blockOperation];
+    [self addExecutionBlock];
 }
 
 /**
@@ -37,6 +37,34 @@
     NSBlockOperation *op = [NSBlockOperation blockOperationWithBlock:^{
         NSLog(@"执行 task2 --- 当前所在线程：%@", [NSThread currentThread]);
     }];
+    [op start];
+}
+
+/**
+ * 使用子类 NSBlockOperation
+ * 调用方法 addExecutionBlock:
+ */
+- (void)addExecutionBlock {
+    NSBlockOperation *op = [NSBlockOperation blockOperationWithBlock:^{
+        NSLog(@"执行 task3 --- 当前所在线程：%@", [NSThread currentThread]);
+    }];
+
+    [op addExecutionBlock:^{
+        NSLog(@"执行 task4 --- 当前所在线程：%@", [NSThread currentThread]);
+    }];
+    [op addExecutionBlock:^{
+        NSLog(@"执行 task5 --- 当前所在线程：%@", [NSThread currentThread]);
+    }];
+    [op addExecutionBlock:^{
+        NSLog(@"执行 task6 --- 当前所在线程：%@", [NSThread currentThread]);
+    }];
+    [op addExecutionBlock:^{
+        NSLog(@"执行 task7 --- 当前所在线程：%@", [NSThread currentThread]);
+    }];
+    [op addExecutionBlock:^{
+        NSLog(@"执行 task8 --- 当前所在线程：%@", [NSThread currentThread]);
+    }];
+
     [op start];
 }
 
