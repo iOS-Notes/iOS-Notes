b774f1493672b6f1213cde9156888d5b5edde46b
sunjinshuai <s_mike@163.com>
2019-10-23 11:34:43 +0800

    mainQueue

--- sourcecode/Thread/Operation/Operation/ViewController.m
+++ sourcecode/Thread/Operation/Operation/ViewController.m
@@ -16,8 +16,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-
-    [self addExecutionBlock];
+    
+    [self mainQueue];
 }
 
 /**
@@ -35,7 +35,7 @@
  */
 - (void)blockOperation {
     NSBlockOperation *op = [NSBlockOperation blockOperationWithBlock:^{
-        NSLog(@"执行 task2 --- 当前所在线程：%@", [NSThread currentThread]);
+        [self task2];
     }];
     [op start];
 }
@@ -46,30 +46,95 @@
  */
 - (void)addExecutionBlock {
     NSBlockOperation *op = [NSBlockOperation blockOperationWithBlock:^{
-        NSLog(@"执行 task3 --- 当前所在线程：%@", [NSThread currentThread]);
+        [self task3];
     }];
-
+    
     [op addExecutionBlock:^{
-        NSLog(@"执行 task4 --- 当前所在线程：%@", [NSThread currentThread]);
-    }];
-    [op addExecutionBlock:^{
-        NSLog(@"执行 task5 --- 当前所在线程：%@", [NSThread currentThread]);
+        [self task4];
     }];
     [op addExecutionBlock:^{
-        NSLog(@"执行 task6 --- 当前所在线程：%@", [NSThread currentThread]);
+        [self task5];
     }];
     [op addExecutionBlock:^{
-        NSLog(@"执行 task7 --- 当前所在线程：%@", [NSThread currentThread]);
+        [self task6];
     }];
     [op addExecutionBlock:^{
-        NSLog(@"执行 task8 --- 当前所在线程：%@", [NSThread currentThread]);
+        [self task7];
     }];
-
+    [op addExecutionBlock:^{
+        [self task8];
+    }];
+    
     [op start];
+}
+
+- (void)mainQueue {
+    NSOperationQueue *queue = [NSOperationQueue mainQueue];
+    
+    NSInvocationOperation *op1 = [[NSInvocationOperation alloc] initWithTarget:self
+                                                                      selector:@selector(task1)
+                                                                        object:nil];
+    
+    NSInvocationOperation *op2 = [[NSInvocationOperation alloc] initWithTarget:self
+                                                                      selector:@selector(task2)
+                                                                        object:nil];
+    
+    NSInvocationOperation *op3 = [[NSInvocationOperation alloc] initWithTarget:self
+                                                                      selector:@selector(task3)
+                                                                        object:nil];
+    
+    NSBlockOperation *op4 = [NSBlockOperation blockOperationWithBlock:^{
+        [self task4];
+    }];
+    
+    [op4 addExecutionBlock:^{
+        [self task5];
+    }];
+    
+    [queue addOperation:op1];
+    [queue addOperation:op2];
+    [queue addOperation:op3];
+    [queue addOperation:op4];
+    
+    [queue addOperationWithBlock:^{
+        [self task6];
+    }];
+    
+    [queue addOperationWithBlock:^{
+        [self task7];
+    }];
 }
 
 - (void)task1 {
     NSLog(@"执行 task1 --- 当前所在线程：%@", [NSThread currentThread]);
+}
+
+- (void)task2 {
+    NSLog(@"执行 task2 --- 当前所在线程：%@", [NSThread currentThread]);
+}
+
+- (void)task3 {
+    NSLog(@"执行 task3 --- 当前所在线程：%@", [NSThread currentThread]);
+}
+
+- (void)task4 {
+    NSLog(@"执行 task4 --- 当前所在线程：%@", [NSThread currentThread]);
+}
+
+- (void)task5 {
+    NSLog(@"执行 task5 --- 当前所在线程：%@", [NSThread currentThread]);
+}
+
+- (void)task6 {
+    NSLog(@"执行 task6 --- 当前所在线程：%@", [NSThread currentThread]);
+}
+
+- (void)task7 {
+    NSLog(@"执行 task6 --- 当前所在线程：%@", [NSThread currentThread]);
+}
+
+- (void)task8 {
+    NSLog(@"执行 task8 --- 当前所在线程：%@", [NSThread currentThread]);
 }
 
 @end
