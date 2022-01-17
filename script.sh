90d46c576c9efddcf51af525c44631b63c380cf7
sunjinshuai <s_mike@163.com>
2022-01-17 11:49:34 +0800

    fix：添加脚本

---
+++ script.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+git filter-branch --env-filter '
+OLD_EMAIL="sunjinshuai@mengxiang.com"
+CORRECT_NAME="sunjinshuai"
+CORRECT_EMAIL="s_mike@163.com"
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
